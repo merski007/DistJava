cdec7c97a218188dfaffebb894d2a278ed17f053
mjmersenski <mjmersenski@qg.com>
2018-02-13 20:56:25 -0600

    Add the real FirstWebApp

---
+++ FirstWebApp/src/edu/wctc/dj/first/FirstServlet.java
@@ -0,0 +1,17 @@
+package edu.wctc.dj.first;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
