456fab797904c9497b488f117e9ab5051788bd8b
mjmersenski <mjmersenski@qg.com>
2018-02-16 15:15:51 -0600

    added a bone structure for product service.

---
+++ JavaCyclesJSTL/src/main/java/edu/wctc/dj/week4/model/ProductService.java
@@ -0,0 +1,67 @@
+package edu.wctc.dj.week4.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductService {
+    private List<ProductStrategy> products;
+    
+    
+    
+//    
+//    private List<Bike> bikeList = Arrays.asList(
+//            new Bike("1","Trek","Trek Madone 9.9","The bike the Pros ride!","1478000_2018_A_1_MADONE_9_9_C_H2.jpg",11999.99),
+//            new Bike("2","Trek","Domane SLR 9 Disc","Our most comfortable bike, now with disc brakes.","1477400_2018_D_1_Domane_SLR_8_Disc.jpeg",10100.88),
+//            new Bike("3","Trek","Boone 7 Disc","Our lightest XC bike to date!","2466000_2018_A_1_Boone_7_Disc.jpeg",7000.00)
+//    );
+//
+//    
+//    public Bike getBike(String id){
+//        Bike bike = null;
+//        for (Bike x : bikeList){
+//            if(x.getId().equals(id)){
+//                bike = x;
+//            }
+//        }
+//        return bike;
+//    }
+//    
+//    public List<Bike> getAllBikes(){
+//        return bikeList;
+//    }
+    
+    
+//    public Bike getName(String id) {
+//        Bike theName = null;
+//        for (Bike bike : bikeList) {
+//            if (bike.getId().equals(id)) {
+//                theName = bike;
+//            }
+//        }
+//        return theName;
+//    }
+//    
+//    public List<Bike> getAllNames() {
+//        return bikeList;
+//    }
+//    
+//    public List<Bike> findNames(String search) {
+//        search = search.toLowerCase();
+//        List<Bike> theList = new ArrayList<>();
+//        for(Bike bike: bikeList){
+//            if(bike.getName().toLowerCase().startsWith(search)){
+//                theList.add(bike);
+//            }
+//        }
+//        return theList;
+//    }
+    
+//    public static void main(String[] args) {
+//        ProductService bike = new ProductService();
+//        System.out.println(bike.getBike("1").getModelName());
+//        System.out.println(bike.getAllBikes().size());
+//    }
+}
+
+
