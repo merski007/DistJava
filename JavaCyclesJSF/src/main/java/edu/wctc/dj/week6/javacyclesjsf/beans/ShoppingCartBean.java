d5c55b2bd82237024d281f3e57ba7060f268f73e
mjmersenski <mjmersenski@qg.com>
2018-03-06 19:26:19 -0600

    continue work on the shoppingCartBean. right now it is just a bones structure

---
+++ JavaCyclesJSF/src/main/java/edu/wctc/dj/week6/javacyclesjsf/beans/ShoppingCartBean.java
@@ -0,0 +1,13 @@
+package edu.wctc.dj.week6.javacyclesjsf.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+@Named(value = "shoppingCartBean")
+@Dependent
+public class ShoppingCartBean {
+
+    public ShoppingCartBean() {
+    }
+    
+}
