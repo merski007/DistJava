b9aa1c7e11402991a59abf19590d8abd9b0a420a
Mark Mersenski <mjmersenski@qg.com>
2018-03-20 18:23:11 -0500

    copied the CartBean and ShoppingCart models from namesapp8 over to this project

---
+++ JavaCyclesJSF/src/main/java/edu/wctc/dj/week6/javacyclesjsf/beans/CartBean.java
@@ -0,0 +1,34 @@
+package edu.wctc.dj.week6.javacyclesjsf.beans;
+
+import edu.wctc.dj.week6.javacyclesjsf.model.Product;
+import edu.wctc.dj.week6.javacyclesjsf.model.ShoppingCart;
+import edu.wctc.dj.week6.javacyclesjsf.model.ShoppingCartService;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
+@Named(value = "cartBean")
+@SessionScoped
+public class CartBean implements Serializable {
+
+    private final String sessionId;
+    private final ShoppingCart cart;
+    private final ShoppingCartService cartService = new ShoppingCartService();
+    
+    public CartBean() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        sessionId = facesContext.getExternalContext().getSessionId(true);
+        
+        cart = cartService.getContents(sessionId);
+    }
+    
+    public int getItemsInCart(){
+        return cart.getItemsInCart();
+    }
+    
+    public void addToCart(Product prod){
+        cart.add(prod);
+        cartService.update(sessionId, cart);
+    }
+}
---
+++ JavaCyclesJSF/src/main/java/edu/wctc/dj/week6/javacyclesjsf/model/ShoppingCart.java
@@ -0,0 +1,34 @@
+package edu.wctc.dj.week6.javacyclesjsf.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart implements Serializable {
+    private List<Product> contents = new ArrayList<>();
+
+    public List<Product> getContents() {
+        return contents;
+    }
+
+    public void setContents(List<Product> contents) {
+        this.contents = contents;
+    }
+    
+    public int getItemsInCart(){
+        return contents.size();
+    }
+    
+    public void add(Product prod){
+        contents.add(prod);
+    }
+    
+    public void remove(Product prod){
+        contents.remove(prod);
+    }
+    
+    public void removeAll(){
+        contents.clear();
+    }
+    
+}
