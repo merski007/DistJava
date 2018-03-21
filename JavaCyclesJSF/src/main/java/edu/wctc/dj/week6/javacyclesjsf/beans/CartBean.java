2cfd701c462a1aea366ec6dbb191e786cbeb69e5
Mark Mersenski <mjmersenski@qg.com>
2018-03-20 19:16:17 -0500

    i can add products to the cartBean and navigate to the shopping cart via the navbar link, but when i get to the shopping cart page there is no data. The page displays the correct number of rows, but it doesn't show the product data

--- JavaCyclesJSF/src/main/java/edu/wctc/dj/week6/javacyclesjsf/beans/CartBean.java
+++ JavaCyclesJSF/src/main/java/edu/wctc/dj/week6/javacyclesjsf/beans/CartBean.java
@@ -6,6 +6,7 @@ import edu.wctc.dj.week6.javacyclesjsf.model.ShoppingCartService;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.context.FacesContext;
 
 @Named(value = "cartBean")
@@ -15,19 +16,33 @@ public class CartBean implements Serializable {
     private final String sessionId;
     private final ShoppingCart cart;
     private final ShoppingCartService cartService = new ShoppingCartService();
-    
+    private List<Product> shoppingCart;
+
     public CartBean() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         sessionId = facesContext.getExternalContext().getSessionId(true);
-        
+
         cart = cartService.getContents(sessionId);
     }
-    
-    public int getItemsInCart(){
+
+    public int getItemsInCart() {
         return cart.getItemsInCart();
     }
-    
-    public void addToCart(Product prod){
+
+    public String getContents() {
+        shoppingCart = cart.getContents();
+        return "shoppingCart";
+    }
+
+    public List<Product> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(List<Product> shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void addToCart(Product prod) {
         cart.add(prod);
         cartService.update(sessionId, cart);
     }
