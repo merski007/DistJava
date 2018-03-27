2126921890a7d52aa91e1631238f4c0a71998f61
Mark Mersenski <mjmersenski@qg.com>
2018-03-27 18:40:21 -0500

    created a NamesApp9, which is a copy of NamesApp8, but for some reason the nameList.xhtml is not working

---
+++ NamesApp9/src/main/java/edu/wctc/dj/week9/namesapp9/model/NameService.java
@@ -0,0 +1,40 @@
+package edu.wctc.dj.week9.namesapp9.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NameService {
+
+    private List<Name> nameList = Arrays.asList(
+            new Name("1", "Fred", "Jones",
+                    new Address("123 main st", "milwaukee", "wi", "53223")),
+            new Name("2", "Patrick", "Cudahy",
+                    new Address("456 cedar st", "cudahy", "wi", "50172"))
+    );
+
+    public Name getName(String id) {
+        Name theName = null;
+        for (Name name : nameList) {
+            if (name.getId().equals(id)) {
+                theName = name;
+            }
+        }
+        return theName;
+    }
+
+    public List<Name> getAllNames() {
+        return nameList;
+    }
+
+    public List<Name> findNames(String search) {
+        search = search.toLowerCase();
+        List<Name> theList = new ArrayList<>();
+        for (Name name : nameList) {
+            if (name.getFirst().toLowerCase().startsWith(search) || name.getLast().toLowerCase().startsWith(search)) {
+                theList.add(name);
+            }
+        }
+        return theList;
+    }
+}
