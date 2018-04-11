78be03223e277f0b282111d3dda80f9c2c66cda7
Mark Mersenski <mjmersenski@qg.com>
2018-04-10 19:15:45 -0500

    added a method to interface to pull all like items

--- JavaCycles/src/main/java/edu/wctc/dj/javacycles/service/ProductService.java
+++ JavaCycles/src/main/java/edu/wctc/dj/javacycles/service/ProductService.java
@@ -6,30 +6,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.startsWith;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class ProductService {
-    
+
     @Autowired
     private IProductDAO productDao;
 
     public Product getProduct(String prodId) {
-        // TODO
         return productDao.findOne(prodId);
     }
 
     public List<Product> getAllProducts() throws Exception {
-        // TODO
         return productDao.findAll();
     }
 
     public List<Product> getAllLikeProducts(String type) throws Exception {
         // TODO, starts areound 1:50:55 mark in the video
-        return null;
+        //ExampleMatcher matcher = ExampleMatcher.matching().withMatcher(type, startsWith().ignoreCase());
+//        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("type", startsWith().ignoreCase());
+//
+//        Product product = new Product();
+//        product.setType(type);
+//        return productDao.findAll(Example.of(product, matcher));
+        return productDao.findByType(type);
     }
 
 }
