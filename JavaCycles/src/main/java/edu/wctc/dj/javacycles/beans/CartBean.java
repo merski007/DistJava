46b286a89a26e0aae69fdb6693a803fda091fe79
Mark Mersenski <mjmersenski@qg.com>
2018-04-17 18:00:39 -0500

    removed commented out code and formatted the files

--- JavaCycles/src/main/java/edu/wctc/dj/javacycles/beans/CartBean.java
+++ JavaCycles/src/main/java/edu/wctc/dj/javacycles/beans/CartBean.java
@@ -14,63 +14,50 @@ import org.springframework.stereotype.Component;
 @Component("cartBean")
 @Scope("session")
 public class CartBean implements Serializable {
-    
+
     private final String sessionId;
     private final ShoppingCart cart;
     private final ShoppingCartService cartService = new ShoppingCartService();
     private List<Product> productList;
-    //private Map productList;
     private double cartTotal = 0.0;
 
     public CartBean() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         sessionId = facesContext.getExternalContext().getSessionId(true);
-        
+
         cart = cartService.getContents(sessionId);
     }
-    
+
     public int getItemsInCart() {
         return cart.getItemsInCart();
     }
-    
+
     public String getContents() {
         productList = cart.getContents();
-        //productList = (List<Product>) cartService.getContents(sessionId);
         return "shoppingCart";
     }
-    
+
     public int getQty(Product prod) {
         return cart.getQty(prod);
     }
-    
-//    public double getTotalPrice(Product prod){
-//        double tempTotal;
-//        double tempPrice = prod.getPrice();
-//        double tempQty = (double) cart.getQty(prod);
-//        
-//        tempTotal = tempPrice * tempQty;
-//        
-//        addToCartTotal(tempTotal);
-//        
-//        return tempTotal;
-//    }
-    
-    public double getProdSubtotal (Product prod){
+
+    public double getProdSubtotal(Product prod) {
         double subtotal;
         double price = prod.getPrice();
         double qty = (double) cart.getQty(prod);
-        
+
         subtotal = price * qty;
-        
+
         return subtotal;
-    }    
-    private void addToCartTotal(Product prod){
+    }
+
+    private void addToCartTotal(Product prod) {
         double subtotal = prod.getPrice();
         double temp = cartTotal + subtotal;
         setCartTotal(temp);
     }
-    
-    private void substractFromCartTotal(Product prod){
+
+    private void substractFromCartTotal(Product prod) {
         double subtotal = getProdSubtotal(prod);
         double temp = cartTotal - subtotal;
         setCartTotal(temp);
@@ -83,43 +70,27 @@ public class CartBean implements Serializable {
     public void setCartTotal(double cartTotal) {
         this.cartTotal = cartTotal;
     }
-    
-    
-    
-    
 
-    /*
-    public String getContents() {
-        productList = cart.getContents().stream().distinct().collect(Collectors.toList());
-        return "shoppingCart";
-    }
-     */
     public List<Product> getProductList() {
         return productList;
     }
 
-//    public Map getProductList() {
-//        return productList;
-//    }
     public void setProductList(List<Product> productList) {
         this.productList = productList;
     }
 
-//    public void setProductList(Map productList) {
-//        this.productList = productList;
-//    }
     public void addToCart(Product prod) {
         cart.add(prod);
         addToCartTotal(prod);
         cartService.update(sessionId, cart);
     }
-    
+
     public void clearCart() {
         cart.removeAll();
         setCartTotal(0.0);
         cartService.update(sessionId, cart);
     }
-    
+
     public void removeProduct(Product prod) {
         substractFromCartTotal(prod);
         cart.remove(prod);
--- JavaCycles/src/main/java/edu/wctc/dj/javacycles/model/ShoppingCart.java
+++ JavaCycles/src/main/java/edu/wctc/dj/javacycles/model/ShoppingCart.java
@@ -9,30 +9,16 @@ import java.util.Map;
 
 public class ShoppingCart implements Serializable {
 
-    //private List<Product> contents = new ArrayList<>();
     private Map<Product, Integer> contents = new HashMap<>();
 
-    /**
-     * what i'd like to is store a map with <product,qty> and then look to see
-     * if the product already exists, if yes, then i can up the qty, if no, then
-     * i can add the product to the map
-     *
-     * @return
-     */
-    //Map section
-//    public Map getContents() {
-//        return contents;
-//    }
-    
-    public List<Product> getContents(){
+    public List<Product> getContents() {
         List<Product> keys = new ArrayList<>(contents.keySet());
         return keys;
     }
-    
-    public int getQty(Product prod){
+
+    public int getQty(Product prod) {
         return contents.get(prod);
     }
-    
 
     public void setContents(Map contents) {
         this.contents = contents;
@@ -40,24 +26,19 @@ public class ShoppingCart implements Serializable {
 
     public int getItemsInCart() {
         int temp = 0;
-        for(int x : contents.values()){
+        for (int x : contents.values()) {
             temp += x;
         }
-        
-        //return contents.size();
+
         return temp;
     }
 
     public void add(Product prod) {
         if (!contents.containsKey(prod)) {
             contents.put(prod, 1);
-        } 
-        else if (contents.containsKey(prod)) {
+        } else if (contents.containsKey(prod)) {
             contents.put(prod, contents.get(prod) + 1);
         }
-        
-
-        //contents.add(prod);
     }
 
     public void remove(Product prod) {
@@ -68,29 +49,4 @@ public class ShoppingCart implements Serializable {
         contents.clear();
     }
 
-    /*List section
-    public List<Product> getContents() {
-        return contents;
-    }
-
-    public void setContents(List<Product> contents) {
-        this.contents = contents;
-    }
-    
-    public int getItemsInCart(){
-        return contents.size();
-    }
-    
-    public void add(Product prod){
-        contents.add(prod);
-    }
-    
-    public void remove(Product prod){
-        contents.remove(prod);
-    }
-    
-    public void removeAll(){
-        contents.clear();
-    }
-     */
 }
