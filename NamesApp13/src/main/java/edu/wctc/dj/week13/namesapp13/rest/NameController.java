d4f722a675a8dbdd51ca6dbd15cf120e8310ef6e
Mark Mersenski <mjmersenski@qg.com>
2018-04-24 19:24:15 -0500

    following along in class

---
+++ NamesApp13/src/main/java/edu/wctc/dj/week13/namesapp13/rest/NameController.java
@@ -0,0 +1,34 @@
+package edu.wctc.dj.week13.namesapp13.rest;
+
+import edu.wctc.dj.week13.namesapp13.model.Name;
+import edu.wctc.dj.week13.namesapp13.service.NameService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+    @Autowired
+    private NameService nameService;
+    
+    @RequestMapping(method = POST, path = "/name")
+    public void createName(@RequestBody Name name){
+        nameService.createName(name);
+    }
+    
+    @RequestMapping(method = GET, path = "/name")
+    public List<Name> getNameList() throws Exception{
+        return nameService.getAllNames();
+    }
+    
+    @RequestMapping(method = GET, path = "/name/{id}")
+    public Name getName(@PathVariable String id){
+        Name name = nameService.getName(id);
+        return name;
+    }
+}
